2d33724038b5d4bfd71702397f2c1f80b6c348a9
Denis RL. <demixruizx@gmail.com>
2026-08-14 16:55:04 -0500

    FUNCIONES CON VALOR DE TABLA - MULTIPLES SENTENCIAS

---
+++ 3. Transact-SQL/2. Functions/11.0 Funciones de tabla_MultiplesSentencias.sql
@@ -0,0 +1,20 @@
+ --FUNCIONES CON VALOR DE TABLA - MULTIPLES SENTENCIAS
+
+ -- Sintaxis Funcion Tabla Multiples Sentencias
+CREATE OR ALTER FUNCTION jllb.fn_nombrefuncion
+(
+	--@Parametro TipoDato
+	@edad int
+)
+RETURNS @Resultado TABLE
+(
+	Columna1 tipodato,
+	Columna2 tipodato,
+	Columna3 tipodato
+)
+AS
+BEGIN
+	-- Instrucciones SQL
+	RETURN;
+END;
+go
